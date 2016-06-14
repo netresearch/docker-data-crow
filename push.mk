87e104923e98fe18859fe484351a3be1e36b4e12
Sebastian Mendel <sebastian.mendel@netresearch.de>
2016-06-14 17:45:20 +0200

    make: fix/simplify tagging commands

--- push.mk
+++ push.mk
@@ -4,13 +4,13 @@ REGISTRY_NR   = couchi.nr:5001
 .PHONY: push stable
 
 push:
-	docker tag -f ${DOCKER_REPO}:testing ${DOCKER_REPO}:${TAG}
-	docker tag -f ${DOCKER_REPO}:${TAG} ${REGISTRY_NR}/${DOCKER_REPO}:${TAG}
-	docker push ${REGISTRY_NR}/${DOCKER_REPO}:${TAG}
-	docker tag -f ${DOCKER_REPO}:${TAG} ${REGISTRY_NR}/${DOCKER_REPO}:latest
-	docker push ${REGISTRY_NR}/${DOCKER_REPO}:latest
+	docker tag ${DOCKER_REPO}:testing ${DOCKER_REPO}:${TAG}
+	docker tag ${DOCKER_REPO}:testing ${DOCKER_REPO}:latest
+	docker tag ${DOCKER_REPO}:testing ${REGISTRY_NR}/${DOCKER_REPO}:${TAG}
+	docker tag ${DOCKER_REPO}:testing ${REGISTRY_NR}/${DOCKER_REPO}:latest
+	docker push ${REGISTRY_NR}/${DOCKER_REPO}
 
 stable:
-	docker tag -f ${DOCKER_REPO}:testing ${DOCKER_REPO}:${TAG}
-	docker tag -f ${DOCKER_REPO}:${TAG} ${REGISTRY_NR}/${DOCKER_REPO}:stable
-	docker push ${REGISTRY_NR}/${DOCKER_REPO}:stable
+	docker tag ${DOCKER_REPO}:testing ${DOCKER_REPO}:stable
+	docker tag ${DOCKER_REPO}:testing ${REGISTRY_NR}/${DOCKER_REPO}:stable
+	docker push ${REGISTRY_NR}/${DOCKER_REPO}
